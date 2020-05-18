62294d3595e3044337aed8af85ef25e9b2ef0fb0
장성현 <jseonghyeon@wemakeprice.com>
2020-05-18 17:45:44 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:manrajang/manrajang.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:manrajang/vue-img-fade-scroll.git master:gh-pages
+git push -f git@github.com:manrajang/vue-image-fade-scroll.git master:gh-pages
 
 cd -
